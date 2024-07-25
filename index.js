d412c1584ea56765e2633f62fb5aa93a9db7250d
0Anish0 <anishpandey4576@gmail.com>
2024-07-25 19:32:59 +0530

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const emailRoutes = require('./routes/emailRoutes');
+const emailRoutes = require('./Routes/emailRoutes');
 require('dotenv').config();
 
 const app = express();
